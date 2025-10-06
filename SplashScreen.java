998aa9a010e8bdfd6782dafff0a134f47e3f16ff
HasemMasud <132145645+HasemMasud@users.noreply.github.com>
2025-10-07 00:23:24 +0300

    Add files via upload

---
+++ SplashScreen.java
@@ -0,0 +1,54 @@
+package com.bongoacademy.webview_pro;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    Animation Splash_top,Splash_bottom;
+    ImageView splash_logo,splash_logo2;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        splash_logo = findViewById(R.id.splash_logo);
+        splash_logo2 = findViewById(R.id.splash_logo2);
+
+        Splash_top = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        Splash_bottom = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        splash_logo.setAnimation(Splash_top);
+        splash_logo2.setAnimation(Splash_bottom);
+
+
+        //For Splash Screen
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Code here
+
+                Intent myIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(myIntent);
+                finish();
+
+            }
+        },3000);
+
+
+    } // OnCreate Method End Here =================
+
+
+} // Public Class End Here ========================
